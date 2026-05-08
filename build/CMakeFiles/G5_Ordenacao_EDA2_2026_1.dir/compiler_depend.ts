b43d4a301e6d30c75dc944d3c74d6608a956fe90
HenriqueFontenelle <fontenelle.dec@gmail.com>
2026-05-07 22:40:32 -0300

    fix: adiciona build/ ao gitignore e remove do rastreamento

--- build/CMakeFiles/G5_Ordenacao_EDA2_2026_1.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for G5_Ordenacao_EDA2_2026_1.
--- build/CMakeFiles/G5_Ordenacao_EDA2_2026_1.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for G5_Ordenacao_EDA2_2026_1.
-# This may be replaced when dependencies are built.
